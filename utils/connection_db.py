25474c4d4cdfdd7ab1473c9e8be4e0909034084f
Aldemitar <aldemarcatanocardona@gmail.com>
2025-05-05 18:26:57 -0500

    Se agrego el modelo para la conexión a la base de datos

--- utils/connection_db.py
+++ utils/connection_db.py
@@ -4,6 +4,7 @@ from sqlalchemy.ext.asyncio import create_async_engine, AsyncEngine
 from sqlmodel import SQLModel
 from sqlmodel.ext.asyncio.session import AsyncSession
 from sqlalchemy.orm import sessionmaker
+from data.models import Usuario
 
 CLEVER_DB = "postgresql+asyncpg://u60dt4rwua0a1f2kobje:xf2x9Y6pH7mzY6iaRCUvoLgva2QMEf@blcwpgvpmuk2l5l48ymd-postgresql.services.clever-cloud.com:50013/blcwpgvpmuk2l5l48ymd"
 
