a564b451b9b9f21f014f4ab4cc560c12680dde57
Aldemitar <aldemarcatanocardona@gmail.com>
2025-05-05 18:24:41 -0500

    Se agrego los datos para la conexión a la base de datos y se hizo unos ajustes

--- utils/connection_db.py
+++ utils/connection_db.py
@@ -4,11 +4,8 @@ from sqlalchemy.ext.asyncio import create_async_engine, AsyncEngine
 from sqlmodel import SQLModel
 from sqlmodel.ext.asyncio.session import AsyncSession
 from sqlalchemy.orm import sessionmaker
-from data.models import Usuario
 
 CLEVER_DB = "postgresql+asyncpg://u60dt4rwua0a1f2kobje:xf2x9Y6pH7mzY6iaRCUvoLgva2QMEf@blcwpgvpmuk2l5l48ymd-postgresql.services.clever-cloud.com:50013/blcwpgvpmuk2l5l48ymd"
-
-# DATABASE_URL= "sqlite+aiosqlite:///petsdb.db"
 
 engine : AsyncEngine = create_async_engine(CLEVER_DB, echo=True)
 async_session =sessionmaker(engine, class_=AsyncSession, expire_on_commit=False)
