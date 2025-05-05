e93ab569ba4a75a58b4fbd23d33b725f1fc5c1f9
Aldemitar <aldemarcatanocardona@gmail.com>
2025-05-05 17:01:59 -0500

    Se agregó el modelo de usuario para la base de datos

--- data/models.py
+++ data/models.py
@@ -1,4 +1,17 @@
-'''Aqui debes consignar el modelo que se te indico en el parcial
-Escribe aquí el que te corresponde.
+from sqlalchemy import Column, String, Integer, Boolean, Enum
+from sqlalchemy.orm import Mapped, mapped_column
+import enum
 
-'''
+class EstadoEnum(enum.Enum):
+    Activo = "Activo"
+    Inactivo = "Inactivo"
+    Eliminado = "Eliminado"
+
+class Usuario(Base):
+    __tablename__ = "usuarios"
+    
+    id: Mapped[int] = mapped_column(primary_key=True, autoincrement=True)
+    nombre: Mapped[str] = mapped_column(String(50), nullable=False)
+    email: Mapped[str] = mapped_column(String(100), nullable=False, unique=True)
+    estado: Mapped[EstadoEnum] = mapped_column(Enum(EstadoEnum), nullable=False)
+    premium: Mapped[bool] = mapped_column(Boolean, nullable=False)
