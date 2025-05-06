8dbb524ec01f430e912adc6234dcde8e4a9225e7
Sala 1 <sala1@FI-LabSistemas.ucatolica.loc>
2025-05-06 11:24:20 -0500

    Se agrego el nuevo modelo para tareas

--- data/models.py
+++ data/models.py
@@ -1,10 +1,19 @@
-from sqlmodel import SQLModel, Field
+from sqlmodel import SQLModel, Field, Relationship
+from typing import Optional
+from datetime import datetime
+from sqlalchemy import Enum as SqlEnum
 import enum
 
 class EstadoEnum(str, enum.Enum):
     Activo = "Activo"
     Inactivo = "Inactivo"
     Eliminado = "Eliminado"
+
+class EstadoTarea(enum.Enum):
+    pendiente = "Pendiente"
+    en_ejecucion = "En ejecucion"
+    realizada = "Realizada"
+    cancelada = "Cancelada"
 
 class Usuario(SQLModel, table=True):
     __tablename__ = "usuarios"
@@ -14,3 +23,12 @@ class Usuario(SQLModel, table=True):
     email: str = Field(index=True, unique=True)
     estado: EstadoEnum
     premium: bool
+
+class Tarea(SQLModel, table=True):
+    id: Optional[int] = Field(default=None, primary_key=True)
+    nombre: str = Field(max_length=100, nullable=False)
+    descripcion: str = Field(max_length=300, nullable=True)
+    fecha_creacion: datetime = Field(default_factory=datetime.utcnow)
+    fecha_modificacion: Optional[datetime] = Field(default_factory=datetime.utcnow)
+    estado: EstadoTarea = Field(sa_column=SqlEnum(EstadoTarea), default=EstadoTarea.pendiente)
+    usuario_id: int = Field(foreign_key="usuarios.id", nullable=False)
