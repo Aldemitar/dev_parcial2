1c0878d15c0f8f157210ad59474155f9ddbcd8af
Aldemitar <aldemarcatanocardona@gmail.com>
2025-05-05 21:24:43 -0500

    Se actualizo los datos del CLEVER CLOUD

--- utils/connection_db.py
+++ utils/connection_db.py
@@ -6,7 +6,7 @@ from sqlmodel.ext.asyncio.session import AsyncSession
 from sqlalchemy.orm import sessionmaker
 from data.models import Usuario
 
-CLEVER_DB = "postgresql+asyncpg://u60dt4rwua0a1f2kobje:xf2x9Y6pH7mzY6iaRCUvoLgva2QMEf@blcwpgvpmuk2l5l48ymd-postgresql.services.clever-cloud.com:50013/blcwpgvpmuk2l5l48ymd"
+CLEVER_DB="postgresql+asyncpg://upeaaahcees6r1uha5dj:sUa4rcAheEZGmJLLitewJbsCWaBq65@boik0dmexa61wv34gl6t-postgresql.services.clever-cloud.com:50013/boik0dmexa61wv34gl6t"
 
 engine : AsyncEngine = create_async_engine(CLEVER_DB, echo=True)
 async_session =sessionmaker(engine, class_=AsyncSession, expire_on_commit=False)
