d6ab88046c7b44d5ed1b3f56cbaa41643e881b75
Aldemitar <aldemarcatanocardona@gmail.com>
2025-05-05 19:06:33 -0500

    Se agregaron las operaciones para llevar a cabo los endpoints de crear usuario y obtener todos los usuarios en el main.py

--- operations/operations_db.py
+++ operations/operations_db.py
@@ -1,1 +1,23 @@
-'''Aqui debes construir las operaciones que se te han indicado'''
+from sqlalchemy.ext.asyncio import AsyncSession
+from sqlalchemy.exc import IntegrityError
+from sqlalchemy.future import select
+from fastapi import HTTPException, status
+from typing import List
+
+from data.models import Usuario
+from data.schemas import UsuarioCreate
+
+async def crear_usuario_db(usuario: UsuarioCreate, session: AsyncSession) -> Usuario:
+    nuevo_usuario = Usuario(**usuario.dict())
+    session.add(nuevo_usuario)
+    try:
+        await session.commit()
+        await session.refresh(nuevo_usuario)
+        return nuevo_usuario
+    except IntegrityError:
+        await session.rollback()
+        raise HTTPException(status_code=400, detail="Correo ya registrado")
+
+async def obtener_usuarios_db(session: AsyncSession) -> List[Usuario]:
+    result = await session.execute(select(Usuario))
+    return result.scalars().all()
