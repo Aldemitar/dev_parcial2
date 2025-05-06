cb7fbadb76ee6703f416d3a460b6013019f904d7
Aldemitar <aldemarcatanocardona@gmail.com>
2025-05-06 18:14:16 -0500

    Se agregó nuevos modelos para la creacion de tareas y lecturas de estas mismas

--- data/schemas.py
+++ data/schemas.py
@@ -1,8 +1,25 @@
 from sqlmodel import SQLModel
-from data.models import EstadoEnum
+from data.models import EstadoEnum, EstadoTarea
+from datetime import datetime
+from typing import Optional
 
 class UsuarioCreate(SQLModel):
     nombre: str
     email: str
     estado: EstadoEnum
     premium: bool
+
+class TareaCreate(SQLModel):
+    nombre: str
+    descripcion: Optional[str] = None
+    estado: Optional[EstadoTarea] = EstadoTarea.pendiente
+    usuario_id: int
+
+class TareaRead(SQLModel):
+    id: int
+    nombre: str
+    descripcion: Optional[str]
+    fecha_creacion: datetime
+    fecha_modificacion: datetime
+    estado: EstadoTarea
+    usuario_id: int
